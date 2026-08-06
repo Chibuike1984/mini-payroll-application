2872c58797aa5ac6f27ff3278f8f35e3301f819c
KizsbrownOG <sponkibrownO.G@gmail.com>
2026-08-06 19:08:03 +0100

    made some changes with the table

--- Resourses/javascript/payroll.js
+++ Resourses/javascript/payroll.js
@@ -52,25 +52,4 @@ $("#Cancel").click(function(){
 
 
 
-$(document).ready(function()
-    {
-    
-    $("#eyeslash").click(function()
-    {
-    $("#edd").dialog(
-        { position:{my: "button", at: "center", of: window},
-        height:"auto", width:450,padding:0,margin:0,/* title: "New Employee"*/show: { effect: 'slide', direction: "up" }
-        , hide: { effect: 'slide', direction: "up" },modal:true, buttons: [
-    
-]});
-}
-)});
-
-$(document).ready(function()
-{
-$("#Cancel").click(function(){
-    $("#ed").dialog("close")
-})
-}
-);
 
